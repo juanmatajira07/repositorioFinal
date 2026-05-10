2f102021a3fe9700f462c9687c21847de3b1b707
juanmatajira07 <juan.matajira07@gmail.com>
2026-05-10 12:43:42 -0500

    Ejercicio 1 y 2 parcial

---
+++ parcial1/soluciones_p1.py
@@ -0,0 +1,17 @@
+#Ejercicio_1
+
+def f1_intermedia(f, a, b):
+    suma_total = 0
+    for i in range(a, b + 1):
+        suma_total += f(i) * i
+    return suma_total
+
+#Ejercicio_2
+
+def f2_intermedia(L):
+    def polinomio(x):
+        resultado = 0
+        for i, coef in enumerate(L):
+            resultado += coef * (x ** i)
+        return resultado
+    return polinomio
