359e3c461722463a44d0b6ede725d3b493257d26
JPMCUEC <juan.isaziga@est.uexternado.edu.co>
2026-05-12 17:39:20 -0500

    Actualizando soluciones de parciales y poo

--- parcial2/soluciones_p2.py
+++ parcial2/soluciones_p2.py
@@ -12,4 +12,39 @@ def tresfibonacci(n):
         yield a
         a, b, c = b, c, a + b + c
 
-# faltan ejercicio 2 y 3
+#Ejercicio 2
+def f2(func):
+    errores   = [0]
+    bloqueado = [False]
+
+    def wrapper(*args):
+        if bloqueado[0]:
+            print('Leer la documentacion')
+            return None
+
+        try:
+            return func(*args)         
+        except Exception as e:
+            errores[0] += 1
+            print(f'Ha cometido {errores[0]} error(es): {type(e).__name__}')
+            if errores[0] >= 3:         
+                bloqueado[0] = True
+            return None                 
+    return wrapper
+
+@f2
+def division(a, b):
+    return a / b
+
+# --- RECURSIÓN ---
+def f3i(n):
+    """Versión iterativa para obtener el primer dígito."""
+    while n >= 10:
+        n = n // 10
+    return n
+
+def f3r(n):
+    """Versión recursiva para obtener el primer dígito."""
+    if n < 10:          
+        return n
+    return f3r(n // 10)
